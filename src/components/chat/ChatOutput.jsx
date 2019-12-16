f4cd54f5c05208631803f123ff842f81cd818f77
ZoZTravolta <zoharsaghi@gmail.com>
2019-12-16 18:46:26 +0200

    convert chatOutput to function conmponent

--- src/components/chat/ChatOutput.jsx
+++ src/components/chat/ChatOutput.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import ChatOutputItem from "./ChatOutputItem";
 
-class ChatOutput extends React.Component {
-   constructor(props) {
-      super(props);
-      // this.setState = {
-      //    tweets: []
-      // };
-   }
-
-   render() {
-      const tweets = this.props.tweets;
-      return (
-         <ul className="ChatOutputUl">
-            {tweets.map(tweet => {
-               return <ChatOutputItem key={tweet.id} tweet={tweet} />;
-            })}
-         </ul>
-      );
-   }
-}
+const ChatOutput = props => {
+   const tweets = props.tweets;
+   return (
+      <ul className="ChatOutputUl">
+         {tweets.map(tweet => {
+            return <ChatOutputItem key={tweet.id} tweet={tweet} />;
+         })}
+      </ul>
+   );
+};
 export default ChatOutput;
